b1090bf9c3a95506da5977360a6e0060901ecd90
Pattarawadee <vrhappytogether@gmail.com>
2025-12-08 15:56:16 +0700

    landing page sections career

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        'desktop': '1920px',
+      },
+      fontFamily: {
+        rubik: ["Rubik", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+};
+
